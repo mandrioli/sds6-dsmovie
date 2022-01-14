464c64d9379ab9c2ddeca2063bfcb77004ae085a
mandrioli <marcelandrioli@gmail.com>
2022-01-14 18:02:35 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,14 +1,22 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useState, useEffect } from "react";
+import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/requests";
 
 function Listing() {
 
-    // FORMA ERRADA
-    axios.get(`${BASE_URL}/movies`)
-        .then(response => {console.log(response.data)});
+    const [pageNumber, setPageNumber] = useState(0);
 
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=10&page=1`)
+        .then(response => { 
+            const data = response.data as MoviePage;
+            console.log(response.data);
+            setPageNumber(data.number); 
+        });
+    }, []);
 
     return (
         <>
